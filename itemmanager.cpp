dc81524f489d84931b3ad156abe560a9018bc3e7
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-07-13 17:30:26 +0200

    new commit

--- itemmanager.cpp
+++ itemmanager.cpp
@@ -1,5 +1,22 @@
 #include "itemmanager.h"
 
+ItemManager* ItemManager::sInstance = nullptr;
+
 ItemManager::ItemManager(QObject *parent)
     : QObject{parent}
 {}
+
+ItemManager* ItemManager::getInstance() {
+    if (!sInstance) {
+        sInstance = new ItemManager();
+    }
+
+    return sInstance;
+}
+
+ItemManager::~ItemManager() {
+    qDeleteAll(mItems);
+    mItems.clear();
+    qDeleteAll(mBackup);
+    mBackup.clear();
+}
