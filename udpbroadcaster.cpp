5877cfbe572f196574017997d908d84b78846ff3
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-07-14 13:55:34 +0200

    new commit

--- udpbroadcaster.cpp
+++ udpbroadcaster.cpp
@@ -15,25 +15,52 @@ UdpBroadcaster::~UdpBroadcaster() {
 
 void UdpBroadcaster::startBroadcasting()
 {
-
+    stopBroadcasting();
+    if (isRunning()) {
+        quit();
+        wait();
+    }
 }
 
 void UdpBroadcaster::stopBroadcasting()
 {
-
+    if (!mBroadcasting) {
+        mBroadcasting = true;
+        start();
+    }
 }
 
 void UdpBroadcaster::run()
 {
+    mSocket = new QUdpSocket();
+    mTimer = new QTimer();
 
+    connect(mTimer, &QTimer::timeout, this, &UdpBroadcaster::broadcastData);
+
+    mTimer->start(5000);
+
+    exec();
+
+    delete mSocket;
+    delete mTimer;
+    mSocket = nullptr;
+    mTimer = nullptr;
 }
 
 void UdpBroadcaster::broadcastData()
 {
+    if (!mBroadcasting || mSocket) {
+        return;
+    }
+    QString xmlData = generateXMLData();
+    QByteArray data = xmlData.toUtf8();
 
+    mSocket->writeDatagram(data, QHostAddress::Broadcast, 12345);
+
+    emit broadcastSent(xmlData);
 }
 
 QString UdpBroadcaster::generateXMLData()
 {
-
+    //QString xml = "<?xml"
 }
