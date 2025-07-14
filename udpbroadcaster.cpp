3a90aa7d1b0256d54efb37a2b491344bc78cf506
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-07-14 14:05:55 +0200

    creation of udp broadcaster definition

--- udpbroadcaster.cpp
+++ udpbroadcaster.cpp
@@ -62,5 +62,30 @@ void UdpBroadcaster::broadcastData()
 
 QString UdpBroadcaster::generateXMLData()
 {
-    //QString xml = "<?xml"
+    QString xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    xml += "<transactions>\n";
+
+    TransactionManager *transactionManager = TransactionManager::getInstance();
+    QList<Transaction*> transactions = transactionManager->getTransactions();
+
+    for (Transaction *transaction : transactions) {
+        xml += "  <transaction>\n";
+        xml += QString("   <customer>%1</customer>\n").arg(transaction->getCustomer()->getName());
+        xml += QString("   <datetime>%1</datetime>\n").arg(transaction->getDateTime().toString(Qt::ISODate));
+        xml += "   <items>\n";
+
+        for (const PurchaseItem &item : transaction->getItems()) {
+            xml += "   <item>\n";
+            xml += QString("    <name>%1</name>\n").arg(item.item->getName());
+            xml += QString("    <quantity>%1</quantity>\n").arg(item.quantity);
+            xml += "    </item>\n";
+        }
+
+        xml += "   </items>\n";
+        xml += "  </transaction>\n";
+    }
+
+    xml += "</transactions>";
+
+    return xml;
 }
