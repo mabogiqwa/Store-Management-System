cba55da4ec7abd5e9d213546281069efc1403be3
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-07-15 14:34:25 +0200

    creating main window definition

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -111,22 +111,71 @@ void MainWindow::setupUI()
 
 void MainWindow::setupMenus()
 {
+    mFileMenu = menuBar()->addMenu("&File");
 
+    mExitAction = new QAction("&Exit", this);
+    mExitAction->setShortcut(QKeySequence::Quit);
+    mExitAction->setStatusTip("Exit the application");
+    mFileMenu->addAction(mExitAction);
+
+    mDataMenu = menuBar()->addMenu("&Data");
+
+    mAddCustomerAction = new QAction("&Add Customer", this);
+    mAddCustomerAction->setShortcut(QKeySequence("Ctrl+U"));
+    mAddCustomerAction->setStatusTip("Add a new customer.");
+    mDataMenu->addAction(mAddItemAction);
+
+    mDataMenu->addSeparator();
+
+    mCreateTransactionAction = new QAction("Create &Transaction", this);
+    mCreateTransactionAction->setShortcut(QKeySequence("Ctrl+T"));
+    mCreateTransactionAction->setStatusTip("Create a new transaction");
+    mDataMenu->addAction(mCreateTransactionAction);
+
+    mDataMenu->addSeparator();
+
+    mRestoreItemsAction = new QAction("&Restore Items", this);
+    mRestoreItemsAction->setStatusTip("Restore items from backup");
+    mDataMenu->addAction(mRestoreItemsAction);
+
+    //Network Menu
+    mNetworkMenu = menuBar()->addMenu("&Network");
+
+    mStartBroadcastAction = new QAction("&Start Broadcasting", this);
+    mStartBroadcastAction->setStatusTip("Start broadcasting transaction data");
+    mNetworkMenu->addAction(mStartBroadcastAction);
+
+    mStopBroadcastAction = new QAction("&Stop Broadcasting", this);
+    mStopBroadcastAction->setStatusTip("Stop broadcasting transaction data");
+    mStopBroadcastAction->setEnabled(false);
+    mNetworkMenu->addAction(mStopBroadcastAction);
+
+    //Add help menu later
 }
 
 void MainWindow::setupToolBar()
 {
+    mToolBar = addToolBar("Main Toolbar");
 
+    mToolBar->addAction(mAddCustomerAction);
+    mToolBar->addAction(mAddItemAction);
+    mToolBar->addSeparator();
+    mToolBar->addAction(mCreateTransactionAction);
+    mToolBar->addSeparator();
+    mToolBar->addAction(mStartBroadcastAction);
+    mToolBar->addAction(mStopBroadcastAction);
 }
 
 void MainWindow::setupStatusBar()
 {
-
+    mStatusBar = statusBar();
+    mStatusBar->showMessage("ready");
 }
 
 void MainWindow::setupConnections()
 {
-
+    //menu actions
+    connect(mAddCustomerAction, &QAction::triggered, this, &MainWindow::onAddCustomer);
 }
 
 void MainWindow::updateActions()
